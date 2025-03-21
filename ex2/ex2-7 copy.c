6c636cfa0260f66840d00c2660c7a31bcf1b2ccf
Lee hak-jun <74577825+lhj0954@users.noreply.github.com>
2025-03-21 21:26:53 +0900

    Create ex2-7 copy.c

---
+++ ex2/ex2-7 copy.c
@@ -0,0 +1,12 @@
+/*문자와 문자열 데이터의 출력*/
+#include <stdio.h>
+
+int main(void) {
+    printf("%c\n", 'A');
+    printf("%s\n", "A");
+    printf("%c은 %s입니다.\n", '1', "first");
+
+    return 0;
+}
+
+//c-문자 s-문자열
